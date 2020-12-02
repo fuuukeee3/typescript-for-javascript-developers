caf6b259790aaef87e6473362520f1fab943e8fa
fu <semarusika@gmail.com>
2020-12-03 07:27:17 +0900

    type array

---
+++ src/array.ts
@@ -0,0 +1,14 @@
+export {}
+
+let numbers: number[] = [1, 2, 3]
+let numbers2: Array<number> = [1, 2, 3]
+
+let strings: Array<string> = ['tokyo', 'osaka', 'fukuoka']
+let strings2: string[] = ['tokyo', 'osaka', 'fukuoka']
+
+let nijiArray: number[][] = [
+  [50, 100],
+  [150, 30],
+]
+
+let array: (string | number | boolean)[] = [1, false, 'japan']
