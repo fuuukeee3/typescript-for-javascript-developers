0c5166a9d240c345c3e370ee6ee67478aeb77209
fu <semarusika@gmail.com>
2020-12-17 08:02:56 +0900

    enum

---
+++ src/enum.ts
@@ -0,0 +1,28 @@
+export {}
+
+enum Months {
+  January = 1,
+  Feburary,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(Months.November)
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  BLACK = '#000000',
+}
+
+console.log(COLORS.GREEN)
